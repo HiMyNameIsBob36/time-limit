adfb84b1209922b53c3c8777bbb5179caa541ed6
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-10 16:39:22 +1100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,15 +1,15 @@
 {
 	"format_version": 2,
 	"header": {
-		"name": "§aAdvance",
-		"description": "§aAdvance is a minecraft mod that lets you play for a chosen amout of time!\nStuck? You can use /function help for a list of commands!",
+		"name": "§l§aAdvance",
+		"description": "§aAdvance§r is a minecraft mod that lets you play for a chosen amout of time!\nStuck? You can use /function help for a list of commands!",
 		"uuid": "187f50b2-5d6d-4ab7-a7f1-b2f761799e4b",
 		"version": [1, 0, 0],
 		"min_engine_version": [1, 20, 0]
 	},
 	"modules": [
 		{
-			"description": "Behavior",
+			"description": "data",
 			"version": [1, 0, 0],
 			"uuid": "187f50b2-5d6d-4ab7-a7f1-b2f761799e4b",
 			"type": "data"
